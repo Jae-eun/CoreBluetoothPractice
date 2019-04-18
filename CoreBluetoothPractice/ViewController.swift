44f1cee117250f82ea1aec12d372b5cd47b8e59a
Jae-eun <je4297@naver.com>
2019-04-19 00:22:12 +0900

    Initial Commit

---
+++ CoreBluetoothPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreBluetoothPractice
+//
+//  Created by 이재은 on 19/04/2019.
+//  Copyright © 2019 Jaeeun Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
